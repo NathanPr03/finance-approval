c9f445198da32b941fdb027fa3b086ee1c0e5762
Nathan03nable <nathan.pringle@n-able.com>
2024-08-04 16:05:40 +0100

    Add header

--- api/approve_finance.go
+++ api/approve_finance.go
@@ -36,6 +36,9 @@ func ApproveFinance(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:4200")
 	w.Header().Set("Content-Type", "application/json")
 	_ = json.NewEncoder(w).Encode(FinanceResponse{Eligible: mockFinanceApprovalSystem(customerID)})
 }
